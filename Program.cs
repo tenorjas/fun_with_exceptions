8c28867c97caee4370ac41bf33f8e0f927c87f7d
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-16 09:58:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fun_with_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
