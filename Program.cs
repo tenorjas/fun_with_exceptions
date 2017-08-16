b63f4a741b2a726da6df08dd3f80824837469e18
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-16 10:58:14 -0400

    finished the exercize, the program is not supposed to run

--- Program.cs
+++ Program.cs
@@ -6,7 +6,56 @@ namespace fun_with_exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n****\nI'm done!!!\n****\n");
+        }
+
+        public static int divideInts(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch (System.Exception)
+            {
+                
+                Console.WriteLine("Can not divide by 0.");
+                throw;
+            }
+            
+        }
+
+        public static int getSafeInt()
+        {
+                try
+                {
+                    Console.WriteLine("Enter an integer.");
+                    return int.Parse(Console.ReadLine());
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("You did not enter an integer.  Please try again.");
+                    throw;
+                }
+        }
+            
+        
+
+        public static int sumOfNumbers(int[] numbers, int length)
+        {
+            try
+            {
+                int total = 0;
+                foreach (var num in numbers)
+                {
+                    total += num;
+                }
+                return total;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("The operands are not valid.  Please try again.");
+                throw;
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+This was an exercise in using try/catch.  This program does not run, and is not supposed to.
